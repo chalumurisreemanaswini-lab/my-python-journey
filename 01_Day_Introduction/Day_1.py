bbc6fc18c5829bb703aaf4d74cc7ddaae8dc2072
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-01-07 18:22:08 +0000

    Day 1: Setup project structure and practiced active recall of data types
    Successfully set up my GitHub repository for the 30-day challenge. I practiced basic arithmetic and used active recall to memorise data types. So far so good!

---
+++ 01_Day_Introduction/Day_1.py
@@ -0,0 +1,19 @@
+print("Hello, this is the start to my python journey!")
+print(8 + 2)
+print(8 - 2)
+print(8 * 2)
+print(8 / 2)
+print(8 ** 2)
+
+# ---DAY ONE REFLECTION---
+# Today was honestly stressful trying to navigate through the controls of GitHUb to make day one.
+# I struggled trying to set up my account but it worked out in the end after lots of tutorials.
+# Currently I am active recalling the different data types
+
+# MY ACTIVE RECALL LIST:
+# 1. Integer (int) -> Whole numbers like 10 or -5
+# 2. Float (float) -> Numbers with decimals like 9.8 or 3.14
+# 3. String (str) -> Text wrapped in "quotes"
+# 4. Complex -> Numbers with an imaginary part like 1 + 2j
+
+# At the end, I got this set up, the most hardest part for me and im proud I have achieved so!
