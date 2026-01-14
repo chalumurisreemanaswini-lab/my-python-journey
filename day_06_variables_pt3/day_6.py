a389927d6e10a649a55862e6ebc7365a10b568ce
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-01-14 20:30:16 +0000

    DAY 6: FINAL PROJECT FOR VARIABLES
    This is now the final project for variables. I unfortunately I lost all of my work for day 6. But at least I finished a lot more quicker and I had extra recall for Variables. By tomorrow, I will make sure my file/folder has definitely gone through. So I will make sure not to make that mistake again. So far so good!

---
+++ day_06_variables_pt3/day_6.py
@@ -0,0 +1,24 @@
+# ---DAY 6: VARIABLES FINAL PROJECT---
+
+# 1. Get user input:
+user_name = input("Enter your name: ")
+birth_year = input("What year were you born?: ")
+
+# 2. Use "Casting" to turn the year into a number for maths
+age = 2026 - int(birth_year)
+
+# 3. Create a story by 'Concatenating' (joining) Strings
+# We turn 'age' back into a String so it can join the text!
+biography = "Student " + user_name + " is " + str(age) + " years old."
+
+print("---PORTFOLIO REPORT---")
+print(biography)
+print("FOUNDATION: strong")
+
+# ---REFLECTION---
+# It is now day 6! These days are going by like a flash.
+# I unfortunately lost all my work for day 6 and I skipped day 7 for some other reasons.
+# So that would mean I should be on day 7 atleast now but I had to redo all my work for day 6.
+# But that is okay! Atleast I finished a lot more quicker due to the brain's memory cells
+# and I got extra active recall to strengthen my foundation even more!
+# So that isn't something to be frustrated about, it was instead a learning experience.
