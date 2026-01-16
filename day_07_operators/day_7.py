d452d72ff97b66963a50f6e7cc73109e817de870
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-01-16 21:42:18 +0000

    DAY 07: Operators
    I started Operators and it is going well so far! I am glad I kept going and I know I wont be stopping anytime! So far so good

---
+++ day_07_operators/day_7.py
@@ -0,0 +1,25 @@
+# --- DAY 07: OPERATORS ---
+
+# 1. Comparition (True or False?)
+
+print(10 > 5)     # Greater than: True
+print(10 < 5)     # Less than: False
+print(10 == 10)   # Equal to: True (Notice the double ==)
+print(10 != 5)    # Not equal to: True
+
+# 2. String Comparision (Case sensitive!)
+print("apple" == "Apple") # False (The capital 'A' makes it different)
+
+# 3. PRACTICAL
+anna_age = 16
+required_age = 18
+print("Am I old enough to drive?", anna_age >= required_age)
+
+
+# --- REFLECTION ---
+
+# Today is Day 7! I pushed myself to work eventhough I came back exhausted from a birthday party.
+# I have now moved on from Variables, I am mostly confident with Variables 
+# But I will make sure to active recall, once I finish all the main beginner topics for Python,
+# I want to recall all of the topics once again to make sure I am definetly confident before moving on to the harder topics.
+# To conclude, I am glad I picked up the laptop and finished my code!
