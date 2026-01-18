263c7527448e1ac92707c827be0cbd38027cf70b
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-01-18 20:59:37 +0000

    Day 9: Geometry and calculations with operators

---
+++ day_09_operators_final_pt3/day_9.py
@@ -0,0 +1,17 @@
+# --- DAY 9: GEOMETRY AND CALCULATIONS ---
+
+# 1. Calculating the perimeter of a triangle
+# Let's pretend we a measuring a physical object
+side_a = 5
+side_b = 10
+side_c = 8
+perimeter = side_a + side_b + side_c
+
+print("The perimeter of the triangle is:", perimeter)
+
+# 2. Calculating the area of a rectangle
+length = 20
+width = 10
+area = length * width
+
+print("The area of the rectangle is:", area)
