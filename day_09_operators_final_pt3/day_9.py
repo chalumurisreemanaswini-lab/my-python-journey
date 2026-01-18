061d582601e3d01e453720c93f81728cc9c7d5ea
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-01-18 22:00:58 +0000

    Day 9: Add reflection and using comparison operators
    I am getting more and more confident with python. It may not be drastic improvement but the fact that I am improving itself is really exciting. Tomorrow is when I will hit the mile stone of 10 days. I will be entering the double-digits, so far so good!

--- day_09_operators_final_pt3/day_9.py
+++ day_09_operators_final_pt3/day_9.py
@@ -15,3 +15,15 @@ width = 10
 area = length * width
 
 print("The area of the rectangle is:", area)
+
+# 3. Using Comparision + Math
+# Is area larger than 150
+is_large_rectangle = area > 150
+print("Is this a large rectangle?", is_large_rectangle)
+
+# --- RELFECTION ---
+
+# Today is Day 9, I made sure to wrap up my study of operators by applying them to geometry problems.
+# I used arithmetic operators for area and perimeter and then used a comparision operator to validate the data.
+# I am super glad that I am now confident about how math and code work together.
+# Additionally, I am also learning to touch type. It has been about a week and I am seeing improvment!
