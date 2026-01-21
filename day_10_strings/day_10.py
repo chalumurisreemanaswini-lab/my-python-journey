e320fb3e0acbacd574b62e9220d97ef7d5f616b8
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-01-21 21:22:08 +0000

    DAY 10: Strings
    I am now in the double digits! I have also started a new topic. I am sure I will carry on with the topic of "Strings" for a few from now to build a strong understanding

---
+++ day_10_strings/day_10.py
@@ -0,0 +1,25 @@
+# DAY 10: STRINGS
+# (Now in the double digits!)
+
+# 1. The "Shortcut" (f-strings)
+
+name = "Alex"
+day = 10
+# Put an "f" before the start.
+# Then you can just put variables in {curly brackets}!
+message = f"Today is day {day} for {name}"
+
+print(message)
+
+# The "Enter Key" (/n)
+# The /n will force the words onto a new line
+list_of items = "Apples/nBananas/nCherries"
+
+print(list_of_items)
+
+# --- REFLECTION ---
+
+# I am finding the advanced string stuff a bit confusing, so i'm slowing down. 
+# I learned that f"..." is a shortcut to put variables in text without using plus signs.
+# I also learned that /n is like pressing the Enter key inside a sentence.
+# So I learned a lot today and I will continue this topic tomorrow
