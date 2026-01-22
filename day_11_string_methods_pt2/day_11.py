7d92de838a9ed497f97ba83b52b989e079419b76
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-01-22 18:40:32 +0000

    Day 11: Practicing String Methods
    Today I learned to demonstrate string transformation methods in Python. I am a day into double digits now and I am still proud and keeping track while going in my own pace. So far so good!

---
+++ day_11_string_methods_pt2/day_11.py
@@ -0,0 +1,24 @@
+# DAY 11: THE "TRANSFORMATION" BUTTONS
+
+# 1. The "SHOUTING" Button (.upper())
+# This turns everything into capital letters.
+
+message = "i am learning"
+print(message.upper())    # RESULT: I AM LEARNING
+
+# 2. The "whispering" Button (.lower())
+# This turns everything into lowercase (great for fixing messy user input)
+
+name = "ALEX"
+print(name.lower())   # RESULT: alex
+
+# 3. The "Length" Button (len())
+# This counts every letter and space
+
+word = "Python"
+print(len(word))   # RESULT: 6
+
+# Today I decided to keep it simple and practice string 'methods'.
+# I learned multiple things. How to change text into all caps or lowercase
+# using .upper() and .lower().
+# I am still keeping track of my progress and I know I will keep it up!
