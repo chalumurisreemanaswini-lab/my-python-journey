9a134c3268e6b51610971d4809f2a74ef125b597
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-01-24 18:22:31 +0000

    Day 12: Checking the data
    Today I have added examples of string methods for data validation. I learned how you could use them for real life purposes as well. So far so good!

---
+++ day_12_strings_pt3/day_12.py
@@ -0,0 +1,29 @@
+# --- DAY 12: CHECKING THE DATA ---
+
+# 1. Is it all letters? (.isalpha())
+# Useful for if you ask for a name and a user types in numbers.
+name = "Alex"
+print(name.isalpha())   # RESULT: True (It is all letters)
+
+password = "Alex123" 
+print(password.isalpha.=())   # RESULT: False (It has numbers)
+
+# 2. Is it all numbers (.isdigit())
+# Useful for when you ask for a phone number.
+phone = "12345"
+print(phone.isdigit())   # RESULT: True 
+
+phone_text = "call me"
+print(phone_text.isdigit())   # RESULT: False
+
+# 3. Does it start with...? (.startswith())
+email = "test@gmail.com"
+print(email.startswith("test"))   # RESULT: True
+
+
+# --- REFLECTION ---
+# Day 12 and feeling proud of my streak. I had to miss yesterday since I had a lot of homeworks and assignments. 
+# But that is ok, as long as I am back in the game right?
+# So today I worked with simple True/False checks.
+# I learned about methods like .isalpha() and .isdigit().
+# These are crucial for data checks. I learned a lot and how to use it for actual purposes!
