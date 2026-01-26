d65a03daaef0d1c5e704c0a9cac330a0e6a02865
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-01-26 20:18:29 +0000

    Day 14: Strings Uniform Trick
    Today, I cleaned user data to lowercase. I decided to keep it simple and it all worked out at the end achieving my sense of satisfaction of completing yet another day. So far so good!

---
+++ day_14_strings_cleaning_pt5/day_14.py
@@ -0,0 +1,24 @@
+# --- DAY 14: TTHE UNIFORM TRICK ---
+# Also applying the knowledge the things I have learned back on the Day 11 file.
+
+# Dirty data (Mixed up capitals)
+user_1 = "ALEX"
+user_2 = "alex"
+user_3 = "ALEx"
+
+# Clean the data by making them all lowercase
+clean_1 = user_1.lower()
+clean_2 = user_2.lower()
+clean_3 = user_3.lower()
+
+# Now they match!
+print(clean_1 == clean_2 == clean_3)   # RESULT: True
+
+# --- RELFECTION ---
+
+# Today I decided to apply what I had learned before as more of a use that can be user in real life.
+# I was starting to get overwhelmed, searching how a code I was planning to do works.
+# I wanted to skip today for a break since I was starting to get upset with how little I felt like I was learning.
+# But I realised just doing a simple code consistently is far better than multiple codes in one day and a long break.
+# So what better idea than to apply knowledge from a few days ago!
+
