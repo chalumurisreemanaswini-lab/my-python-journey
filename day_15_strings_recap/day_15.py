2604d416c88f8143a4f5b329e374669ef8866b00
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-01-27 22:08:23 +0000

    Day 15: Strings and subject recap
    I decided to recall the key parts and write them down onto one file. I have kept easy point short and easy to understand. This will make revising far more easier and less overwhelming. So far so good!

---
+++ day_15_strings_recap/day_15.py
@@ -0,0 +1,57 @@
+# --- DAY 15: STRINGS AND TOPIC RECAP ---
+# To active recall and revise the most topics that I have learned so far!
+# These are all in simple easy-to-understand codes!
+
+# 1. Creating strings
+first_name = "Alex"
+last_name = "Smith" 
+country = "Australia"
+
+print(first_name)
+print(last_name)
+print(country)
+
+# 2. String length
+word = "Python"
+print(len(word))   # Counts characters
+
+# 3. String concatenation
+first_name = "Alex"
+last_name = "Smith"
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+# 4. Using f-strings
+language = "Python"
+version = 3
+
+print(f"I am learning {language} version {version}")
+
+# 5. String indexing
+word = "Python"
+
+print(word[0])   # P
+print(word[3])   # h
+print(word[-1])   # n
+
+# 6. String slicing
+word = "Python"
+
+print(word[0:4])   # Pyth
+print(word[2:])   # thon
+print(word[:3])   # Pyt
+
+# 7. Common string methods
+text = "I love python"
+
+print(text.upper())
+print(text.capitalize())
+print(text.count("o"))
+print(text.replace("python", "coding"))
+
+# --- RELFECTION ---
+# Today I decided to active recall the key parts and write them down onto one file.
+# This is so that it will be easier revise and to make sure I am keepipng track.
+# It is also now officially past 2 weeks!
+# I am very proud I have made it this far, my next mile stone will be when I reach 1 month!
