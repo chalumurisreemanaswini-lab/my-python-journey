bb38e6f8f56810329aff4c1dc1e7d415fb70d86a
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-02-02 21:50:39 +0000

    Day 19: Introduction to lists
    Finally the starting of a new topic. Today I learned basic list operations. I am starting to get a feel for what lists are and how you could use them in programming. So far so good!

---
+++ day_19_introduction_to_lists/day_19.py
@@ -0,0 +1,33 @@
+# --- DAY 19: INTRODUCTION TO LISTS ---
+
+# Creating a list
+fruits = ["apple", "banana", "orange"]
+
+print(fruits)
+
+# Acessing list items (just like strings)
+fruits = ["apple", "banana", "orange"]
+print(fruits[0])   # apple
+print(fruits[-1])   # orange
+
+# 3. Length of a list
+fruits = ["apple", "banana", "orange"]
+print(len(fruits))   # 3
+
+# 4. Adding items to a list
+fruits = ["apple", "banana"]
+fruits.append("orange")
+print(fruits)
+
+# Looping through a list
+fruits = ["apple", "banana", "orange"]
+for fruit in fruits:
+  print(fruit)
+
+# --- REFLECTIONS ---
+
+# Today I learned the basics of lists and how they store multiple values.
+# I decided to keep it simple as a way to get a kickstart on a new topic.
+# It is kinda like dipping my toes in the swimming pool to get a feel for the temperature.
+# (Hah I love writing creatively in english).
+# To conclude, this helped me understand how programs can manage collections of data!
