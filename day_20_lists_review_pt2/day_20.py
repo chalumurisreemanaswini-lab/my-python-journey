8aeb3d4022d6e2fd63f0017965a7e07ee8afa9c7
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-02-04 18:31:24 +0000

    Day 20: Lists review + one new thing
    I am unwell so I decided to keep it simple. I learned how to remove from lists. I am glad I got myself to post and kept up the consistency. So far so good!

---
+++ day_20_lists_review_pt2/day_20.py
@@ -0,0 +1,34 @@
+# --- DAY 20: LISTS REVIEW + ONE NEW THING ---
+
+# 1. Review
+numbers = [1, 2, 3, 4]
+
+print(numbers[0])   # first item
+print(len(numbers))   # length
+
+# 2. One new list method: remove()
+numbers = [1, 2, 3, 4]
+
+numbers.remove(3)
+print(numbers)
+
+# It removes the value you tell it to.
+
+# 3. Lists + strings together (tiny combo)
+names = ["Alex", "Sam", "Jordan"]
+
+for name in names:
+  print(name.upper())
+
+# SHORT MINI TASK
+foods = ["pizza", "pasta", "rice"]
+
+foods.append("soup")
+print(foods)
+
+# --- REFLECTION ---
+
+# Today I decided to keep it light since I am very ill.
+# I recalled things I had learned yesterday and one extra new thing.
+# I learned how to remove from lists.
+# I have learned one new thing and that is already a win for me!
