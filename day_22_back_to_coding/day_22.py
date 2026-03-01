a15166a1cf61fd75d953f043558bee7d21945b9c
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-03-01 15:43:50 +0000

    Day 22: Document comeback plan
    Today I added reflections and reviews on coding after a break. I will make sure to keep posting from now on. I will also remind myself to set up PyCharm and Python. So far so good!

---
+++ day_22_back_to_coding/day_22.py
@@ -0,0 +1,27 @@
+# --- DAY 22: COMEBACK PLAN ---
+
+# NOTE: It has been 3 weeks since I have posted on Github.
+# This was due my illness, busy schedule and a lot going on. 
+# Also because I lacked a laptop during the time period.
+# My parents very kindly bought me a laptop so now I am back in the game!
+
+# 1. REVIEW (Lists + Strings Combo)
+names = ["Alex"], "Jordan", "Sam"]
+
+for name in names:
+  print("name.upper())
+  print("Length:", len(name))
+
+# 2. REVIEW
+sentence = "I am back to coding!"
+
+print(sentence.upper())
+print(sentence[::-1])
+print("coding" in sentence.lower())
+
+# --- REFLECTION ---
+
+# I am glad to be back in coding again! I have some tasks to complete like installing Pycharm, Python and setting up the Github connection cleanly.
+# So I will make sure to complete those. I also downloaded Notion desktop on this laptop and set it up.
+# I love that app and it has proven to be useful every single time.
+# That's about it for today!
