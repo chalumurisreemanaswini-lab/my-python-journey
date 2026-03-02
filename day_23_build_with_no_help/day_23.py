2c437ed72ff9459c898babdac44a279c38e736d5
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-03-02 20:07:20 +0000

    Day 23: BUILDING WITH NO HELP
    Today really helped a lot. I built a code myself then I corrected it. More importantly, I learned more about the mistake and thought of ways to help me avoid it in the future. So far so good!

---
+++ day_23_build_with_no_help/day_23.py
@@ -0,0 +1,26 @@
+# --- DAY 23: BUILDING WITH NO HELP AT ALL ---
+
+# 1. TASK 1
+# My answer:
+animals = ["Tiger", "Zebra", "Bear"]
+
+print(animal.upper())
+
+print("The total animals:", len(animals))
+
+
+
+# Correct answer: 
+animals = ["Tiger", "Zebra", "Bear"]
+
+for animal in animals:
+  print(animal.upper())
+
+print("Total animals:", len(animals))
+
+# --- REFLECTION ---
+
+# I used this to learj that lists contais multiples values,
+# so I need to loop through them to use string methods like upper().
+# I looked into the topic more and now I feel far more confident.
+# This helped clear doubts and helped me understand lists and loops better.
