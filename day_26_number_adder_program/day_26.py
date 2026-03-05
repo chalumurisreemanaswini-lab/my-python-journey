f47728f406a57e73542314e615b0ae8491f1d298
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-03-05 19:11:11 +0000

    Day 26: Number adder program
    I implemented a number adder program using a while loop that continuously adds user input until 0 is entered. I kept it simple yet effective. So far so good!

---
+++ day_26_number_adder_program/day_26.py
@@ -0,0 +1,19 @@
+# --- DAY 26: NUMBER ADDER PROGRAM (WHILE LOOP PRACTCE)
+
+# 1. The program
+total = 0
+number = 1
+
+while number != 0:
+  number = int(input("Enter a number (0 to stop): "))
+  total += number
+  print("Current total:", total)
+
+print("Final total:", total)
+
+# --- REFLECTION ---
+
+# I chose to continue while loops with a program. 
+# I am starting to get the hang of while loops and the different programs you could make with it.
+# I also understand why they are important.
+# One of the topics I have planned to master by the end of march was while loops.
