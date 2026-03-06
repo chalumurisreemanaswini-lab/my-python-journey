40031dfb1b63c08758e920e60112b8aa0377d4a5
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-03-06 20:44:24 +0000

    Day 27: A simple calculator project and a dice roller.
    I implemented a dice roller and a simple calculator in Python. I am planning to continue while loops tomorrow a bit more. So far so good!

---
+++ day_27_fun_projects/day_27.py
@@ -0,0 +1,35 @@
+# --- DAY 27: FUN PROJECTS! ---
+
+# 1. Dice roller
+import random
+
+dice = random.randint(1, 6)
+print("You rolled a", dice)
+
+if dice == 6:
+  print("Wow! You got the highest number.")
+else:
+  ("Try again for a 6")
+
+# 2. Simple calculator
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number."))
+operatation = input("Enter +, -, *, /: ")
+
+if operation == "+":
+  print("Result:", num1 + num2)
+elif operator == "-":
+  print("Result:", num1 - num2)
+elif operator == "*":
+  print("Result:", num1 * num2)
+elif operator == "/":
+  print("Result:", num1 / num2)
+else:
+  print("INVALID OPERATION")
+
+# --- REFLECTION ---
+
+# Today I decided to take a break from while loop. 
+# I made some fun projects from knowledge I had already learned.
+# It was super fun recalling everything I had already learned.
+# Tomorrow I am planning to continue while loops a bit more.
