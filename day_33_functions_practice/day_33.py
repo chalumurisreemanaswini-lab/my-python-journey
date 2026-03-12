0140f6245da8a0242f6fe82e81f84a7b44d3b0f4
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-03-12 16:05:05 +0000

    Day 33: Simple functions practice
    I added functions for greeting, adding, and subtracting numbers. This helped me reinforce the knowledge I had already learned yesterday. So far so good!

---
+++ day_33_functions_practice/day_33.py
@@ -0,0 +1,38 @@
+# --- DAY 33: SIMPLE FUNCTIONS PRACTICE
+
+# 2. Function that says hello
+def day_hello():
+  print("Hello!")
+
+say_hello()
+
+# 2. Function that greets a person
+def greet(name):
+  print("Hello", name)
+
+greet("Alex")
+
+# parameter is (name)
+
+# 3. Function that adds numbers.
+def add(a, b):
+  return a + b
+
+print(add(2, 3))
+
+# MY MINI EXERCISE:
+# make a function called subtract
+def subtract(a, b):
+  return a - b
+
+print(subtract(5, 3))
+
+# --- REFLECTION ---
+
+# I have a party in an hour so I kept this simple yet effective.
+# Today was to reinforce the knowledge I had learned.
+# Thid helped me strengthen what I covered yesterday which was about organizing code into reusable blocks.
+# I will build on to this tomorrow with even more.
+
+
+
