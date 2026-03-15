ce1084edba8166cb8f19eee0d17d6602e1897fbf
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-03-15 21:14:24 +0000

    Day 35: Default parameters
    I learned functions to demonstrate default parameter values so now I can give default values to parameters. Simple but effective. So far so good!

---
+++ day_35_functions_refresher/day_35.py
@@ -0,0 +1,19 @@
+# --- DAY 35: TINY FUNCTIONS REFRESHER (RELAXED VERSION)
+# One new thing
+
+# 1. Default Values in Functions
+# You can give a default value to parameter, so if someone doesn't provide it, Python uses the default
+
+def greet(name="Friend"):
+  print("Hello", name, "!")
+
+greet("Sam")     # Hello Sam !
+greet()          # Hello Friend !
+
+# - name="Friend" = if no argument is given, use "Friend"
+
+# --- REFLECTION ---
+
+# Today I feel exhausted, I atleast managed to fit in a small thing to keep me going.
+# I learned how to give a default value to parameter.
+# I know it is simple but is definetly useful.
