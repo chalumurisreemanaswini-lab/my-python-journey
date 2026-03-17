f3faff33d83d340fe5267129343513b7b6aaa8d3
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-03-17 20:10:50 +0000

    Day 36: Functions continued
    I added functions to check if a number is positive and to describe a number as positive, negative, or zero. This is short but helped me learn something new. So far so good!

---
+++ day_36_functions_continued/day_36.py
@@ -0,0 +1,28 @@
+# --- DAY 36:FUNCTIONS CONTINUED ---
+
+# 1. Function that checks numbers
+def is_positive(n):
+  if n > 0:
+    return True
+  else:
+     return False
+
+  # 2. Function that prints something
+  def describe_number(n):
+    if n > 0:
+      print("Positive")
+    elif n < 0:
+      print("Negative")
+    else:
+      print("Zero")
+
+describe_number(7)
+describe_number(-2)
+describe_number(0)
+
+# --- REFLECTION ---
+
+# Today I practices using functions with if/else statements.
+# I wrote small examples to check the numbers and describe them.
+# This is a short one as school work is piling up but this has definetly taught me something new.
+# Almost at the 40 day mark!
