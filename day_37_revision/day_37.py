0245bd8198242d78fab9f4b73f28d17845a7ce36
sree-chalumuri <chalumuri.ramu@gmail.com>
2026-03-23 16:19:47 +0000

    Day 37:  Revision on Functions
    Today implemented functions to greet and triple a number. I know I missed a few days but I will make sure to slowly get back into coding big commits like before. So far so good!

---
+++ day_37_revision/day_37.py
@@ -0,0 +1,26 @@
+# --- DAY 37: REVISION ON FUNCTIONS AFTER A LONG BREAK ---
+
+# 1. Tiny function
+def say_hi():
+  print("Hi!")
+
+say_hi()
+
+# 2. Slight upgrade
+def say_hi(name):
+  print("Hi", name)
+
+say_hi("ALex")
+
+# 3. Even bigger upgrade
+def triple(n)
+return n * 3
+
+print(triple(3))
+
+# --- REFLECTION: ---
+
+# Yes, I missed a few days of coding and like I was slowing down.
+# Today I focused on restarting with small steps instead of doing much.
+# I learned that consistency is about coming back, not being perfect.
+# I think I will slowly get back into this and will start making bigger commits like a while before!
